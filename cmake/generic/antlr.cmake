24f87c166762e6950e2f36b6068249ff3ea2bb0d
Jeroen D Stout <jeroenstout@gmail.com>
2024-11-28 13:38:23 +0100

    Make antlr generate the default listener instead of a visitor

--- cmake/generic/antlr.cmake
+++ cmake/generic/antlr.cmake
@@ -9,27 +9,25 @@ function(antlr_py_build_single source destination append_generated_files)
   
   set(loc_used_files ${${append_generated_files}})
   list(APPEND loc_used_files "${destination}/${base_out_name}Lexer.py")
-  list(APPEND loc_used_files "${destination}/${base_out_name}Visitor.py")
   list(APPEND loc_used_files "${destination}/${base_out_name}Parser.py")
+  list(APPEND loc_used_files "${destination}/${base_out_name}Listener.py")
   
   message(STATUS "Antlr setup: ${source_relpath} -> ${destination_relpath}")
   message(STATUS " - ${destination}/${base_out_name}Lexer.py")
-  message(STATUS " - ${destination}/${base_out_name}Visitor.py")
   message(STATUS " - ${destination}/${base_out_name}Parser.py")
+  message(STATUS " - ${destination}/${base_out_name}Listener.py")
   
   file(MAKE_DIRECTORY "${abs_gen_script}/${destination}")
   
   add_custom_command(
     COMMENT           "Antlr: ${source} -> ${abs_gen_script}/${destination}/${base_out_name}"
     OUTPUT            "${abs_gen_script}/${destination}/${base_out_name}Lexer.py"
-                      "${abs_gen_script}/${destination}/${base_out_name}Visitor.py"
                       "${abs_gen_script}/${destination}/${base_out_name}Parser.py"
+                      "${abs_gen_script}/${destination}/${base_out_name}Listener.py"
     COMMAND           antlr4
                       -Dlanguage=Python3
                       "${source}"
                       -o "${abs_gen_script}/${destination}/"
-                      -no-listener
-                      -visitor
     DEPENDS           ${source}
     WORKING_DIRECTORY "${abs_gen_script}/${destination}/"
     VERBATIM
